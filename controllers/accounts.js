22def1db5ea80eb55320d423eb3e9c967c07e508
mrandrewlopez <andrewlopezcodes@gmail.com>
2018-05-17 11:27:06 -0400

    created controller structer

---
+++ controllers/accounts.js
@@ -0,0 +1,29 @@
+const accountModel = require('../models/account');
+
+const accountModel = require('../models/account');
+const index = (request, response)=> {
+  const accounts = accountModel.index():
+}
+
+
+
+
+const index = (request, response)=>{}
+
+const show = (request, response)=>{}
+
+const create = (request, response)=>{}
+
+const update = (request, response)=>{}
+
+const destroy = (request, response)=>{}
+
+
+
+modules.exports = {
+  index,
+  show,
+  create,
+  update,
+  destroy
+}
